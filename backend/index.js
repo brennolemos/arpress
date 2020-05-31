f97eb8aed5d848214a2d6d718b16c1c06bfe6caa
Breno Lemos <brenomiros@gmail.com>
2020-05-31 17:41:41 -0300

    Articles and Categories Routers

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ const app = express();
 const bodyParser = require("body-parser");
 const connection = require("./database/database");
 
+const categoriesController = require("./categories/CategoriesController");
+const articlesController = require("./articles/ArticlesController");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -13,6 +16,9 @@ connection
    }).catch((error) => {
       console.log(error);
    })
+
+app.use("/", categoriesController);
+app.use("/", articlesController);
 
 app.get("/", (req, res) => {
    res.send("Bem vindo!");
