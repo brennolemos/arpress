210f39bc54395427be4a0a14ea265d680edc1afe
Breno Lemos <brenomiros@gmail.com>
2020-06-09 18:07:41 -0300

    Configuring TinyMCE

--- frontend/src/components/ArticleForm.jsx
+++ frontend/src/components/ArticleForm.jsx
@@ -1,38 +1,64 @@
 import React from "react";
+import { Editor } from "@tinymce/tinymce-react";
 
-const ArticleForm = () => (
-  <div className="row my-3">
-    <div className="col-md-12">
-      <div className="card">
-        <div className="card-header">
-          <h2>Novo Artigo</h2>
-        </div>
-        <div className="card-body">
-          <form action="http://localhost:8080/categories/save" method="post">
-            <div className="form-group">
-              <input
-                type="text"
-                name="title"
-                placeholder="Defina o título do Artigo"
-                className="form-control"
-              />
-            </div>
-            <div className="form-group">
-              <textarea
+const ArticleForm = () => {
+  const handleEditor = (content, editor) => {
+    console.log(content);
+  };
+
+  return (
+    <div className="row my-3">
+      <div className="col-md-12">
+        <div className="card">
+          <div className="card-header">
+            <h2>Novo Artigo</h2>
+          </div>
+          <div className="card-body">
+            <form action="http://localhost:8080/categories/save" method="post">
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="title"
+                  placeholder="Defina o título do Artigo"
+                  className="form-control"
+                />
+              </div>
+              <div className="form-group">
+                {/* <textarea
                 name="body"
                 id="body"
                 cols="30"
                 rows="5"
                 className="form-control"
                 placeholder="Escreva o artigo"
-              ></textarea>
-            </div>
-            <button className="btn btn-success">Cadastrar</button>
-          </form>
+              ></textarea> */}
+                <Editor
+                className="form-control"
+                  textareaName="body"
+                  initialValue="<p>Escreva o artigo</p>"
+                  init={{
+                    height: 300,
+                    // menubar: false,
+                    plugins: [
+                      "advlist autolink lists link image charmap print preview anchor",
+                      "searchreplace visualblocks code fullscreen",
+                      "insertdatetime media table paste code help wordcount",
+                    ],
+                    toolbar:
+                      "undo redo | formatselect | bold italic backcolor | \
+                      alignleft aligncenter alignright alignjustify | \
+                      bullist numlist outdent indent | removeformat | help",
+                  }}
+                  onEditorChange={handleEditor}
+                />
+              </div>
+              <button className="btn btn-success">Cadastrar</button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ArticleForm;
